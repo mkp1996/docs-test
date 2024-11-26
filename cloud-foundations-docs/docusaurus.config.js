aaaa3d4df524e9a3b200cf70573c6c2be40ebc62
mkp1996 <root@MPTower>
2024-11-26 12:52:22 -0500

    updating config.js

--- cloud-foundations-docs/docusaurus.config.js
+++ cloud-foundations-docs/docusaurus.config.js
@@ -15,15 +15,15 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://mkp1996.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/cloud-docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'mkp1996', // Usually your GitHub org/user name.
+  projectName: 'cloud-docs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
